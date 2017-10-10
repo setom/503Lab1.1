49c36e4c21d992fe0a4748b49d336e2f2358d789
Matt Seto <matthew.y.seto@gmail.com>
2017-10-09 23:05:37 -0700

    Initial Commit

---
+++ website/scripts/basic.js
@@ -0,0 +1,10 @@
+window.onload = function loadNav() {
+	document.getElementById("nav01").innerHTML =
+	"<ul id='menu'>" +
+	"<li><a href='index.html'>Home</a></li>" +
+	"<li><a href='about.html'>About</a></li>" +
+	"<li><a href='maps.html'>Maps</a></li>" +
+	"</ul>";
+	console.log("Fetched Nav");
+}
+ 
